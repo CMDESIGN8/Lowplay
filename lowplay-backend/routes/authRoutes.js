79b75f81990be1f7cd3844fef12641118c55b63c
Chrii90 <christian.mercado@hotmail.com.ar>
2025-04-26 15:58:23 -0300

    zzz

--- lowplay-backend/routes/authRoutes.js
+++ lowplay-backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
 const router = express.Router();
+const verifyToken = require('../middlewares/verifyToken');  // Asegúrate de la ruta correcta
+
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/profile', verifyToken, handler);
+router.get('/profile', verifyToken);
 
 module.exports = router;
