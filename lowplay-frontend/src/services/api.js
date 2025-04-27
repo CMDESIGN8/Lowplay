168e865d8b36a64c2e8064f3d24f75b0917d2577
Chrii90 <christian.mercado@hotmail.com.ar>
2025-04-27 11:34:56 -0300

    k

--- lowplay-frontend/src/services/api.js
+++ lowplay-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://lowplay.onrender.com/api', // Cambia esto si usas otro puerto
+  baseURL: 'https://lowplay.onrender.com', // Cambia esto si usas otro puerto
 });
 
 export default api;
