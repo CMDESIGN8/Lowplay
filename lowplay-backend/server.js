9d9ae792f25fc997ef5f656ae89bc5949b23c65e
Chrii90 <christian.mercado@hotmail.com.ar>
2025-04-26 22:14:38 -0300

    s

--- lowplay-backend/server.js
+++ lowplay-backend/server.js
@@ -42,6 +42,7 @@ app.use(express.json());
 app.use(cors({
     origin: ['https://lowplay-1.onrender.com', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Si usas cookies o credenciales
 }));
 
