1a59e8de109697eb8638c390f44a777cce7bfc84
Chrii90 <christian.mercado@hotmail.com.ar>
2025-04-28 14:03:26 -0300

    g

--- lowplay-frontend/src/components/Dashboard.js
+++ lowplay-frontend/src/components/Dashboard.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // Para mostrar un estado de carga
+  const [error, setError] = useState(null);  // Para manejar errores
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-
+    
     if (!token) {
       navigate('/login');
       return;
@@ -17,14 +19,17 @@ const Dashboard = () => {
     const fetchUserData = async () => {
       try {
         const response = await axios.get('https://lowplay.onrender.com/api/profile', {
-            headers: { 
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-              },
-            });
+          headers: { 
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+        });
         setUser(response.data);
+        setLoading(false);  // Se ha recibido la información, ahora paramos el loading
       } catch (err) {
+        setError('Hubo un error al cargar tu perfil.');
+        setLoading(false); // También detener el loading en caso de error
         console.error('Error fetching profile:', err.response || err.message);
         navigate('/login');
       }
@@ -32,6 +37,14 @@ const Dashboard = () => {
 
     fetchUserData();
   }, [navigate]);
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <div>
@@ -44,7 +57,7 @@ const Dashboard = () => {
           <p>Perfil completado: {user.profile_completed ? 'Sí' : 'No'}</p>
         </>
       ) : (
-        <p>Cargando...</p>
+        <p>No se pudo cargar la información del usuario.</p>
       )}
     </div>
   );
