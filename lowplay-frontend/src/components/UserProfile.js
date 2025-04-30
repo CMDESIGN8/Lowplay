d7fbe7677240c64f56d90896a13cfcdb7fc352c4
Chrii90 <christian.mercado@hotmail.com.ar>
2025-04-30 15:33:03 -0300

    a

--- lowplay-frontend/src/components/UserProfile.js
+++ lowplay-frontend/src/components/UserProfile.js
@@ -14,30 +14,36 @@ const UserProfile = ({ user }) => {
   };
 
   return (
-    <div className="user-profile-card">
-      <div className="user-avatar">
-        <img src={user.avatar || '/assets/avatars/mateo.png'} alt="avatar" />
+    <div className="card-wrapper">
+      <div className={`rank-badge`}>{getBadge(user.level)}</div>
+
+      <div className={`user-profile-card card-border ${user.level?.toLowerCase()}`}>
+        <div className="user-avatar">
+          <img src={user.avatar || '/assets/avatars/mateo.png'} alt="avatar" />
+        </div>
+
+        <div className="user-info">
+          <h2>Bienvenido, {user.username}</h2>
+          <p className="user-level">Ranked {user.level}</p>
+
+          <div className="lowcoins-display">
+            <i className={`fas fa-coins lowcoins-icon animated-coin coin-${user.level.toLowerCase()}`}></i>
+            <span className="lowcoins-count">{user.lowcoins} Lowcoins</span>
+            <span>{user.wallet} WALLET</span>
+            <span>{user.email} EMAIL</span>
+          </div>
+
+          <div className="progress-bar">
+            <div
+              className={`progress-fill ${user.level?.toLowerCase()}`}
+              style={{ width: `${user.progress || 0}%` }}
+            >
+              <span className="progress-text">{user.progress || 0}%</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="user-info">
-        <h2>Bienvenido, {user.username}</h2>
-        <p className="user-level">{getBadge(user.level)} Ranked {user.level}</p>
-        <div className="lowcoins-display">
-  <i className={`fas fa-coins lowcoins-icon animated-coin coin-${user.level.toLowerCase()}`}></i>
-  <span className="lowcoins-count">{user.lowcoins} Lowcoins</span>
-  <span>{user.wallet} WALLET</span>
-  <span>{user.email} EMAIL</span>
-</div>
-
-
-
-<div className="progress-bar">
-<div
-  className={`progress-fill ${user.level?.toLowerCase()}`}
-  style={{ width: `${user.progress || 0}%` }}
->
-    <span className="progress-text">{user.progress || 0}%</span>
-  </div>
-</div></div></div>
+    </div>
   );
 };
 
