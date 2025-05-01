3c61de01305cca4770d8b2e14180525c6efe1f76
Chrii90 <christian.mercado@hotmail.com.ar>
2025-05-01 12:52:44 -0300

    sdfg

--- lowplay-frontend/src/components/UserProfile.js
+++ lowplay-frontend/src/components/UserProfile.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './UserProfile.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Asegurate de tener FontAwesome
+
 
 const UserProfile = ({ user }) => {
   const getBadge = (level) => {
