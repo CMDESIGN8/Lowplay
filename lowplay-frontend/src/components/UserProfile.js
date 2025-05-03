e5c4c50427da37e5304cd438293fe77af0fb2f9a
Chrii90 <christian.mercado@hotmail.com.ar>
2025-05-03 10:01:55 -0300

    456

--- lowplay-frontend/src/components/UserProfile.js
+++ lowplay-frontend/src/components/UserProfile.js
@@ -31,6 +31,15 @@ const UserProfile = ({ user }) => {
         <h2>Bienvenido, {user.username}</h2>
         <p className="user-level">Ranked {user.level}</p>
 
+        <div className="progress-bar">
+          <div className={`progress-fill ${normalizedLevel}`} style={{ width: `${user.progress}%` }}>
+            {user.progress}%
+          </div>
+        </div>
+        <div className="Siguientelvl">
+          <p>Siguiente Nivel </p>
+        </div>
+
         <div className="lowcoins-display">
         <span className="lowcoins-count">LOWCOINS</span>
         </div>
@@ -45,14 +54,7 @@ const UserProfile = ({ user }) => {
   <p><i className="fa-solid fa-wallet"></i> Wallet: {user.wallet}</p>
 </div>
 
-        <div className="progress-bar">
-          <div className={`progress-fill ${normalizedLevel}`} style={{ width: `${user.progress}%` }}>
-            {user.progress}%
-          </div>
-        </div>
-        <div className="Siguientelvl">
-          <p>Siguiente Nivel </p>
-        </div>
+        
       </div>
     </div>
   );
