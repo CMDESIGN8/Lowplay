08d3e69986bce1ae7d3a93a7adc9b4b6faa051b6
Chrii90 <christian.mercado@hotmail.com.ar>
2025-05-09 09:26:21 -0300

    sdf

--- lowplay-frontend/src/components/CanjePremios.js
+++ lowplay-frontend/src/components/CanjePremios.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styles from './CanjePremios.module.css'; // Importa el archivo CSS Module
+import styles from './CanjePremios.css'; // Importa el archivo CSS Module
 import { motion, AnimatePresence } from 'framer-motion';
 
 const CanjePremios = () => {
