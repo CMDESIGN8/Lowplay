af379b4b96c0706b719a6c79cddebf7a82b0daa3
Chrii90 <christian.mercado@hotmail.com.ar>
2025-05-24 18:08:26 -0300

    d

--- lowplay-frontend/src/components/Register.js
+++ lowplay-frontend/src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './Register.css'; // Importamos el nuevo estilo
+import './Register.ss'; // Importamos el nuevo estilo
 
 const Register = () => {
   const [name, setName] = useState('');
