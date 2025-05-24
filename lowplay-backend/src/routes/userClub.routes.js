5b0cf944430eb29c7c510e8aeda5105a963c081c
Chrii90 <christian.mercado@hotmail.com.ar>
2025-05-24 17:53:47 -0300

    sdf

--- lowplay-backend/src/routes/userClub.routes.js
+++ lowplay-backend/src/routes/userClub.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { associateClub, getMyClubs } = require('../controllers/userClub.controller');
-const authenticateToken = require('../middleware/auth');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
 router.post('/asociar', authenticateToken, associateClub);
 router.get('/mis-clubes', authenticateToken, getMyClubs);
