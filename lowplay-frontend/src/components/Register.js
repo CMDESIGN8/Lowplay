7dc2b107f1ab60f6b885d3739c70c5e420ccdc71
Chrii90 <christian.mercado@hotmail.com.ar>
2025-06-20 14:49:11 -0300

    zxc

--- lowplay-frontend/src/components/Register.js
+++ lowplay-frontend/src/components/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post('http://localhost:5000/api/register', {
+      await axios.post('https://lowplay.onrender.com/api/register', {
         name,
         email,
         password,
