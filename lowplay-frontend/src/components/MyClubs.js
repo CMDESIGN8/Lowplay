babd26117df1d2b680b83faf86c06efda064806c
Chrii90 <christian.mercado@hotmail.com.ar>
2025-06-20 16:58:28 -0300

    456

--- lowplay-frontend/src/components/MyClubs.js
+++ lowplay-frontend/src/components/MyClubs.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode'; // npm install jwt-decode
 import './MyClubs.css';
 import { Link } from 'react-router-dom';
 
