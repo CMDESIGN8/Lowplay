b1d6c768c7ddaf6f9dc6d28cff0a73ac99cdb56f
Chrii90 <christian.mercado@hotmail.com.ar>
2025-06-20 15:02:06 -0300

    12

--- lowplay-backend/src/config/db.js
+++ lowplay-backend/src/config/db.js
@@ -2,9 +2,11 @@ const { Pool } = require('pg');
 
 // Usamos las variables de entorno para seguridad
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 });
+
 module.exports = pool;
