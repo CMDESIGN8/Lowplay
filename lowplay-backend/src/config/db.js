f8b47540258811d94cafef35535b25ff3f9de1d8
Chrii90 <christian.mercado@hotmail.com.ar>
2025-06-20 14:57:17 -0300

    sdfg

--- lowplay-backend/src/config/db.js
+++ lowplay-backend/src/config/db.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 
 // Usamos las variables de entorno para seguridad
-const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
