50916fe9974b0a0541b2edb6c656fd449199fd84
Chrii90 <christian.mercado@hotmail.com.ar>
2025-08-15 16:54:47 -0300

    as

--- lowplay-frontend/src/App.js
+++ lowplay-frontend/src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import MyClubs from './components/MyClubs';  
-import Missions from './components/Missions';
+import Missions from './components/Missions'
 
 function App() {
   return (
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mis-clubes" element={<MyClubs />} />
-        <Route path="/missions" element={<Missions />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/mis-clubes" element={<MyClubs />} />
+      <Route path="/missions" element={<Missions />} />
+    </Routes>
   );
 }
 
